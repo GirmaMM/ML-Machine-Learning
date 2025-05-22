eed06a385d62de6a01c2f49b180f6a24ea307656
GirmaMM <60384966+GirmaMM@users.noreply.github.com>
2025-05-22 23:11:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title(' 🤖  Machine Learning App')
 
 st.info('This app bulids a machine learning app')
+
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
